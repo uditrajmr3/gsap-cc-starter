3cb70fbafe65270c80564506bfd8be31d480f0bd
uditrajmr3 <csense2653@gmail.com>
2025-08-12 12:46:29 +0530

    --updated: done with todos

--- src/pages/GsapFrom.jsx
+++ src/pages/GsapFrom.jsx
@@ -1,5 +1,18 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
 const GsapFrom = () => {
   // TODO: Implement the gsap.from() method
+  useGSAP(() => {
+    gsap.from("#green-box", {
+      x: 400,
+      repeat: -1,
+      yoyo: true,
+      rotate: 360,
+      duration: 2,
+      ease: "elastic",
+    });
+  }, []);
 
   return (
     <main>
